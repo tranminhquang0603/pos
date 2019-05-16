24ec55a66becb235882ac6226eacadaf9e039d7f
tranminhquang0603 <mquang0603@gmail.com>
2019-05-16 18:17:01 +0700

    fix

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -23,23 +23,23 @@
 
     <link href="{{ asset('lib/css/customize-template.css')}}" type="text/css" media="screen, projection" rel="stylesheet" />
 
-    <link rel="stylesheet" href="plugins/font-awesome.min.css" >
+    <link rel="stylesheet" href="/plugins/font-awesome.min.css" >
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="plugins/adminlte.min.css">
+  <link rel="stylesheet" href="/plugins/adminlte.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
+  <link rel="stylesheet" href="/plugins/iCheck/flat/blue.css">
   <!-- Morris chart -->
-  <link rel="stylesheet" href="plugins/morris.css">
+  <link rel="stylesheet" href="/plugins/morris.css">
   <!-- jvectormap -->
-  <link rel="stylesheet" href="plugins/jquery-jvectormap-1.2.2.css">
+  <link rel="stylesheet" href="/plugins/jquery-jvectormap-1.2.2.css">
   <!-- Date Picker -->
-  <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
+  <link rel="stylesheet" href="/plugins/datepicker/datepicker3.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker-bs3.css">
+  <link rel="stylesheet" href="/plugins/daterangepicker/daterangepicker-bs3.css">
   <!-- bootstrap wysihtml5 - text editor -->
-  <link rel="stylesheet" href="plugins/bootstrap3-wysihtml5.min.css">
+  <link rel="stylesheet" href="/plugins/bootstrap3-wysihtml5.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
